f4ce041447d03d968bc7749e67fa61dbaf735b6e
Roshni Patel <roshnihpatel@outlook.com>
2022-02-24 15:28:16 +0000

    formatted and lintted

--- challenges/1-longest-word-in-list/longest-word-in-list.js
+++ challenges/1-longest-word-in-list/longest-word-in-list.js
@@ -7,23 +7,35 @@ parameters:
 	- WORDS: list of strings
 
 return type: string
-*/function longestInList(arrayOfStrings){
-    let longest = arrayOfStrings[0]
-    for (let word of arrayOfStrings){
-        if (word.length > longest.length){
-            longest = word
-        }
-
+*/ function longestInList(arrayOfStrings) {
+  let longest = arrayOfStrings[0];
+  for (let word of arrayOfStrings) {
+    if (word.length > longest.length) {
+      longest = word;
     }
-    return longest
+  }
+  return longest;
 }
 
 //test cases
 
-
-console.log('longestInList(["England", "Wales", "Scotland", "Northern Ireland""]):', longestInList(["England", "Wales", "Scotland", "Northern Ireland"]), "expected Northern Ireland")
-console.log('longestInList(["the", "quick", "brown", "fox"]):', longestInList(["the", "quick", "brown", "fox"]), "expected: quick")
-console.log('longestInList(["hello", "hi", "greetings", "hey"]):', longestInList(["hello", "hi", "greetings", "hey"]), "expected: greetings")
-console.log('longestInList(["air nomads", "fire nation", "water tribe", "earth kingdom"]):', longestInList(["air nomads", "fire nation", "water tribe", "earth kingdom"]), "expected: earth kingdom")
-
-
+console.log(
+  'longestInList(["England", "Wales", "Scotland", "Northern Ireland""]):',
+  longestInList(["England", "Wales", "Scotland", "Northern Ireland"]),
+  "expected Northern Ireland"
+);
+console.log(
+  'longestInList(["the", "quick", "brown", "fox"]):',
+  longestInList(["the", "quick", "brown", "fox"]),
+  "expected: quick"
+);
+console.log(
+  'longestInList(["hello", "hi", "greetings", "hey"]):',
+  longestInList(["hello", "hi", "greetings", "hey"]),
+  "expected: greetings"
+);
+console.log(
+  'longestInList(["air nomads", "fire nation", "water tribe", "earth kingdom"]):',
+  longestInList(["air nomads", "fire nation", "water tribe", "earth kingdom"]),
+  "expected: earth kingdom"
+);
